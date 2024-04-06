f742b08697ca0c2e0d118186693a258ed3cbda4c
Eduard Hopfer <hopfere@proton.me>
2024-04-06 18:20:09 +0200

    add namespace to displaycpp

--- board/src/display.cpp
+++ board/src/display.cpp
@@ -7,7 +7,7 @@ const int DISPLAY_LATCH = 4;
 const int DISPLAY_CLOCK = 7;
 const int DISPLAY_DATA = 8;
 
-ShiftDisplay2 get_default() {
+ShiftDisplay2 Display::get_default() {
     ShiftDisplay2 display(
         DISPLAY_LATCH, DISPLAY_CLOCK, DISPLAY_DATA,
         COMMON_ANODE, DISPLAY_SIZE, SWAP_SHIFT_REGISTERS);
