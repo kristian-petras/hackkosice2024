5ed463a782252e09511bbfbd05f48bde30ae4211
podmepodme <petergrocky23@gmail.com>
2024-04-06 13:16:28 +0200

    feat: blink leds

--- board/src/main.cpp
+++ board/src/main.cpp
@@ -1,10 +1,28 @@
 #include <Arduino.h>
 
+const int LED_4 = 13;
+const int _D1_INVERTED = 13;
+const int _D2 = 12;
+const int _D3_INVERTED = 11;
+const int _D4_INVERTED = 10;
+
+void blink_led(int pin, int delay_ms) {
+  digitalWrite(pin, HIGH);
+  delay(delay_ms);
+  digitalWrite(pin, LOW);
+  delay(delay_ms);
+}
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(_D1_INVERTED, OUTPUT);
+  pinMode(_D2, OUTPUT);
+  pinMode(_D3_INVERTED, OUTPUT);
+  pinMode(_D4_INVERTED, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  blink_led(_D1_INVERTED, 1500);
+  blink_led(_D2, 1000);
+  blink_led(_D3_INVERTED, 500);
+  blink_led(_D4_INVERTED, 250);
 }
