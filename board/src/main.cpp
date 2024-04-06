d33f8cabfe2fa8b77967ac0ac2042238c9a63947
podmepodme <petergrocky23@gmail.com>
2024-04-06 14:25:20 +0200

    feat: constants in different file

--- board/src/main.cpp
+++ board/src/main.cpp
@@ -1,16 +1,6 @@
 #include <Arduino.h>
 #include <DebounceEvent.h>
-
-
-const int LED_4 = 13;
-const int _D1_INVERTED = 13;
-const int _D2_INVERTED = 12;
-const int _D3_INVERTED = 11;
-const int _D4_INVERTED = 10;
-
-const int BUTTON_1 = A1;
-const int BUTTON_2 = A2;
-const int BUTTON_3 = A3;
+#include "Constants.h"
 
 DebounceEvent* button;
 
